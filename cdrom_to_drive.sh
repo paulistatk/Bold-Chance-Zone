9046f3c7d39ed62927f22dc59e8cf30f58077bad
paulista <eu@paulista.dev>
2024-02-18 19:06:02 -0300

    modified:   cdrom_to_drive.sh

--- cdrom_to_drive.sh
+++ cdrom_to_drive.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
+
 lsblk
+
+# Verifique se os parâmetros de origem e destino foram fornecidos
+if [ "$#" -ne 2 ]; then
+    echo "Por favor, forneça dois parâmetros: a origem e o destino, conforme saida do lsblk acima."
+    exit 1
+fi
+
 # Mantenha o dispositivo de origem como sr0
-CDROM_DEVICE="/dev/sr0"
+CDROM_DEVICE="/dev/$1"
+if [ -z "$CDROM_DEVICE" ]; then
+    echo "Erro: CDROM_DEVICE não pode ser nulo ou vazio."
+    exit 1
+fi
+echo CDROM_DEVICE $CDROM_DEVICE
+
 CDROM_MOUNT=$(lsblk $CDROM_DEVICE --output MOUNTPOINT -n | head -n1)
+if [ -z "$CDROM_MOUNT" ]; then
+    echo "Erro: CDROM_MOUNT não pode ser nulo ou vazio."
+    exit 1
+fi
+echo CDROM_MOUNT $CDROM_MOUNT
 
 # Detecte o dispositivo de destino montado
-DEST_DEVICE=$(df | grep '/dev/sdb1' | awk '{print $1}')
+DEST_DEVICE=$(df | grep "/dev/$2" | awk '{print $1}')
+if [ -z "$DEST_DEVICE" ]; then
+    echo "Erro: DEST_DEVICE não pode ser nulo ou vazio."
+    exit 1
+fi
+echo DEST_DEVICE $DEST_DEVICE
 
 # Defina o diretório de origem
 DEST_DIR=$(df | grep -m 1 "$DEST_DEVICE" | awk '{print $6}')
-# echo destino $DEST_DIR
+if [ -z "$DEST_DIR" ]; then
+    echo "Erro: DEST_DIR não pode ser nulo ou vazio."
+    exit 1
+fi
+echo DEST_DIR $DEST_DIR
+
+# Restante do script...
 
 # Verifique se o CD-ROM está montado
 if [ -z "$CDROM_MOUNT" ]; then
@@ -31,8 +61,8 @@ echo sudo mount -o loop $ISO_IMAGE $MOUNT_POINT
 sudo mount -o loop $ISO_IMAGE $MOUNT_POINT
 
 # Use rsync para sincronizar os arquivos
+echo rsync -av --progress $MOUNT_POINT/ $DEST_DIR
 rsync -av --progress $MOUNT_POINT/ $DEST_DIR
-
 # Desmonte a imagem ISO
 sudo umount $MOUNT_POINT
 
@@ -40,6 +70,7 @@ sudo umount $MOUNT_POINT
 sudo rm $ISO_IMAGE $LOG_FILE
 
 # Imprima uma mensagem quando a sincronização estiver concluída
-echo "Sincronização concluída!"
+echo "Sincronização concluída! rsync -av --progress $MOUNT_POINT/ $DEST_DIR"
+
 
 eject
