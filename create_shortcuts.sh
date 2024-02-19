0c3c81c1af461a172d13a1f7f63cbf47cb29e63c
paulista <eu@paulista.dev>
2024-02-19 17:51:06 -0300

    modified:   create_shortcuts.sh

--- create_shortcuts.sh
+++ create_shortcuts.sh
@@ -1,30 +1,12 @@
-# ## Usuário
-# script que para cada arquivo executável na pasta crie um atalho em /usr/local/bin
-# ## Bing
-# Claro, você pode usar um script de shell para isso. Aqui está um exemplo:
 
 #!/bin/bash
 
-# Diretório onde estão os arquivos executáveis
-diretorio="$1"
-# Diretório inicial
-dir_inicial="$diretorio"
-
-# Verifica se o diretório existe
-if [ ! -d "$dir_inicial" ]; then
-    echo "O diretório $dir_inicial não existe."
-    exit 1
-fi
-
 # Para cada arquivo no diretório
-for arquivo in "$diretorio"/*; do
+for arquivo in *; do
     # Se o arquivo for executável
     if [[ -x "$arquivo" ]]; then
         # Cria um atalho em /usr/local/bin
-        ln -s `pwd`/$arquivo /usr/local/bin/$arquivo
-        echo $arquivo
+        echo ln -s "`pwd`/$arquivo" /usr/local/bin/$arquivo
+        ln -s "`pwd`/$arquivo" /usr/local/bin/$arquivo
     fi
 done
-
-# Este script irá criar um atalho para cada arquivo executável no diretório especificado em /usr/local/bin. Lembre-se de substituir "/caminho/para/seu/diretorio" pelo caminho do diretório onde estão os arquivos executáveis.
-# Nota: Você precisará de permissões de super
